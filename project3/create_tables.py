b53cca788f1e33dff6af4e5c69ca708becc2c65c
Davide Bersani <davide.bersani@enel.com>
2021-08-13 09:08:19 +0200

    Added project3

---
+++ project3/create_tables.py
@@ -0,0 +1,40 @@
+import configparser
+import logging
+
+import psycopg2
+
+from sql_queries import create_table_queries, drop_table_queries
+
+logging.basicConfig(level=logging.INFO)
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read("dwh.cfg")
+
+    logging.info("Connecting to Redshift cluster...")
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config["CLUSTER"].values()))
+    cur = conn.cursor()
+
+    logging.info("Dropping tables...")
+    drop_tables(cur, conn)
+    logging.info("Creating new tables...")
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
